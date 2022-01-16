cc9f9a348525907f5da22feb9bc24b83188898e7
Nenad Bogojevic <nbogojevic@hotmail.com>
2022-01-16 00:10:55 +0000

    Conf flow refactoring

--- custom_components/midea_dehumidifier_lan/const.py
+++ custom_components/midea_dehumidifier_lan/const.py
@@ -4,6 +4,7 @@ from __future__ import annotations
 from typing import Final
 
 from homeassistant.const import Platform
+
 from midea_beautiful.midea import SUPPORTED_APPS
 
 __version__ = "0.5.0"
@@ -14,24 +15,15 @@ UNIQUE_ID_PRE_PREFIX: Final = "midea_"
 UNIQUE_DEHUMIDIFIER_PREFIX: Final = "midea_dehumidifier_"
 UNIQUE_CLIMATE_PREFIX: Final = "midea_climate_"
 DOMAIN: Final = f"{UNIQUE_DEHUMIDIFIER_PREFIX}lan"
-ISSUE_URL: Final = (
-    "https://github.com/nbogojevic/homeassistant-midea-dehumidifier-lan"
-    "/issues/new/choose"
-)
+# pylint: disable=line-too-long
+ISSUE_URL: Final = "https://github.com/nbogojevic/homeassistant-midea-dehumidifier-lan/issues/new/choose"  # noqa: E501
 
 CONF_ADVANCED_SETTINGS: Final = "advanced_settings"
 CONF_APPID: Final = "appid"
 CONF_APPKEY: Final = "appkey"
-CONF_WHAT_TO_DO: Final = "appliance_action"
 CONF_MOBILE_APP: Final = "mobile_app"
-CONF_BROADCAST_ADDRESS: Final = "broadcast_address"
 CONF_TOKEN_KEY: Final = "token_key"
-CONF_USE_CLOUD: Final = "use_cloud"
-CONF_DETECT_AC_APPLIANCES: Final = "detect_ac_appliances"
-
-TAG_CAUSE: Final = "cause"
-TAG_ID: Final = "id"
-TAG_NAME: Final = "name"
+CONF_USE_CLOUD_OBSOLETE: Final = "use_cloud"
 
 MAX_TARGET_HUMIDITY: Final = 85
 MIN_TARGET_HUMIDITY: Final = 35
@@ -39,11 +31,13 @@ MIN_TARGET_HUMIDITY: Final = 35
 MAX_TARGET_TEMPERATURE: Final = 32
 MIN_TARGET_TEMPERATURE: Final = 16
 
-CURRENT_CONFIG_VERSION: Final = 1
+CURRENT_CONFIG_VERSION: Final = 2
 
 # Wait half a second between successive refresh calls
 APPLIANCE_REFRESH_COOLDOWN: Final = 0.5
 APPLIANCE_REFRESH_INTERVAL: Final = 60
+APPLIANCE_SCAN_INTERVAL: Final = 2
+MIN_SCAN_INTERVAL: Final = 2
 
 ATTR_FAN_SPEED: Final = "fan_speed"
 ATTR_RUNNING: Final = "running"
@@ -53,11 +47,18 @@ PLATFORMS: Final = [
     Platform.CLIMATE,
     Platform.FAN,
     Platform.HUMIDIFIER,
+    Platform.NUMBER,
     Platform.SENSOR,
     Platform.SWITCH,
 ]
 
 UNKNOWN_IP: Final = "0.0.0.0"
+
+# What to do with configured appliance
+DISCOVERY_IGNORE = "IGNORE"
+DISCOVERY_LAN = "LAN"
+DISCOVERY_CLOUD = "CLOUD"
+DISCOVERY_WAIT = "WAIT"
 
 DEFAULT_APP: Final = next(app for app in SUPPORTED_APPS)
 
