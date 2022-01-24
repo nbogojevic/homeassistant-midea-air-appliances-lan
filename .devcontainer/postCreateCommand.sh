e810a2982bc6ae9c7566df29f751e940fa8dcb7f
Nenad Bogojevic <nbogojevic@hotmail.com>
2022-01-24 22:39:10 +0000

    Use compatible iptables

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,6 +3,7 @@
 # Add network testing tools
 apt update
 apt install -y iptools iproute2
+update-alternatives --set iptables /usr/sbin/iptables-legacy
 
 # Fix discontinuing of git support by GitHub
 sed 's/git:/https:/' -i /usr/share/container/upgrade
