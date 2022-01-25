427bd8f8155399ffe5e0cc102fe5a4b754ee005e
Nenad Bogojevic <nbogojevic@hotmail.com>
2022-01-25 15:32:04 +0000

    Add parameters for network testing

--- tests/network-test.sh
+++ tests/network-test.sh
@@ -1,11 +1,17 @@
-echo "Droping packets from $IP_ADDRESS. Sleeping 15 minutes."
-iptables -I INPUT -s $IP_ADDRESS -j DROP
-sleep 900
-iptables -D INPUT -s $IP_ADDRESS -j DROP
-echo "Accepting packets from $IP_ADDRESS. Sleeping 10 minutes."
-sleep 600
-echo "Droping packets to $IP_ADDRESS. Sleeping 15 minutes."
-iptables -I OUTPUT -s $IP_ADDRESS -j DROP
-sleep 900
-iptables -D OUTPUT -s $IP_ADDRESS -j DROP
-echo "Accepting packets from $IP_ADDRESS"
+#! /bin/bash
+IP1=$1
+IP2=${2:-$IP1}
+SLEEP1=${3:-420}
+SLEEP2=${4:-$SLEEP1}
+SLEEP3=${5:-$SLEEP1}
+echo "Droping packets from $IP1. Sleeping $SLEEP1 seconds."
+iptables -I INPUT -s $IP1 -j DROP
+sleep $SLEEP1
+iptables -D INPUT -s $IP1 -j DROP
+echo "Accepting packets from $IP1. Sleeping $SLEEP2 seconds."
+sleep $SLEEP2
+echo "Droping packets to $IP2. Sleeping $SLEEP3 seconds."
+iptables -I OUTPUT -d $IP2 -j DROP
+sleep $SLEEP3
+iptables -D OUTPUT -d $IP2 -j DROP
+echo "Accepting packets from $IP2"
