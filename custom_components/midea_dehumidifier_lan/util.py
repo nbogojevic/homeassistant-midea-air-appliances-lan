bbd3339ab449352282c45f5e19ed5ebd02ae7021
Nenad Bogojevic <nbogojevic@hotmail.com>
2022-01-25 09:23:50 +0000

    Regroup utility classes and functions

--- custom_components/midea_dehumidifier_lan/util.py
+++ custom_components/midea_dehumidifier_lan/util.py
@@ -4,44 +4,57 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from copy import deepcopy
-from typing import Any, Tuple
+from typing import Any, Tuple, cast, final
 
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import (
     CONF_DEVICES,
     CONF_ID,
+    CONF_INCLUDE,
     CONF_PASSWORD,
     CONF_TOKEN,
     CONF_UNIQUE_ID,
     CONF_USERNAME,
 )
 from homeassistant.core import HomeAssistant
+import midea_beautiful as midea_beautiful_api
+from midea_beautiful.appliance import AirConditionerAppliance, DehumidifierAppliance
 from midea_beautiful.cloud import MideaCloud
 from midea_beautiful.lan import LanDevice
+from midea_beautiful.midea import (
+    APPLIANCE_TYPE_AIRCON,
+    APPLIANCE_TYPE_DEHUMIDIFIER,
+    DEFAULT_APP_ID,
+    DEFAULT_APPKEY,
+)
 
-from custom_components.midea_dehumidifier_lan.api import MideaClient
 from custom_components.midea_dehumidifier_lan.const import (
     _ALWAYS_CREATE,
     CONF_TOKEN_KEY,
+    UNKNOWN_IP,
 )
 
+_SUPPORTABLE_APPLIANCES = {
+    APPLIANCE_TYPE_AIRCON: AirConditionerAppliance.supported,
+    APPLIANCE_TYPE_DEHUMIDIFIER: DehumidifierAppliance.supported,
+}
 
-def _redact_key(
-    redacted_data: dict[str, Any], key: str, char="*", length: int = 0
-) -> None:
-    if redacted_data.get(key) is not None:
-        to_redact = str(redacted_data[key])
+
+def _redact(data: dict[str, Any], key: str, char="*", length: int = 0) -> None:
+    """Redacts/obfuscates key in disctionary"""
+    if data.get(key) is not None:
+        to_redact = str(data[key])
         if length <= 0 or length >= len(to_redact):
-            redacted_data[key] = char * len(to_redact)
+            data[key] = char * len(to_redact)
         else:
-            redacted_data[key] = to_redact[:-length] + char * length
+            data[key] = to_redact[:-length] + char * length
 
 
 def _redact_device_conf(device) -> None:
-    _redact_key(device, CONF_TOKEN)
-    _redact_key(device, CONF_TOKEN_KEY)
-    _redact_key(device, CONF_UNIQUE_ID, length=8)
-    _redact_key(device, CONF_ID, length=4)
+    _redact(device, CONF_TOKEN)
+    _redact(device, CONF_TOKEN_KEY)
+    _redact(device, CONF_UNIQUE_ID, length=8)
+    _redact(device, CONF_ID, length=4)
 
 
 class RedactedConf:
@@ -55,8 +68,8 @@ class RedactedConf:
     @property
     def __dict__(self) -> dict[str, Any]:
         conf = deepcopy(self.conf)
-        _redact_key(conf, CONF_USERNAME)
-        _redact_key(conf, CONF_PASSWORD)
+        _redact(conf, CONF_USERNAME)
+        _redact(conf, CONF_PASSWORD)
         _redact_device_conf(conf)
         if conf.get(CONF_DEVICES) and isinstance(conf.get(CONF_DEVICES), list):
             for device in conf[CONF_DEVICES]:
@@ -79,12 +92,49 @@ def is_enabled_by_capabilities(capabilities: dict[str, Any], capability: str) ->
     return False
 
 
-class ApplianceCoordinator(ABC):
+def is_climate(appliance: LanDevice) -> bool:
+    """True if appliance is air conditioner"""
+    return AirConditionerAppliance.supported(appliance.type)
+
+
+def is_dehumidifier(appliance: LanDevice) -> bool:
+    """True if appliance is dehumidifier"""
+    return DehumidifierAppliance.supported(appliance.type)
+
+
+def supported_appliance(conf: dict, appliance: LanDevice) -> bool:
+    """Checks if appliance is supported by integration"""
+    included = conf.get(CONF_INCLUDE, [])
+    for type_id, check in _SUPPORTABLE_APPLIANCES.items():
+        if type_id in included and check(appliance.type):
+            return True
+    return False
+
+
+class ApplianceCoordinator(ABC):  # pylint: disable=too-few-public-methods
     """Abstract interface for Appliance update coordinators"""
 
     appliance: LanDevice
     available: bool
     device: dict[str, Any]
+
+    def is_climate(self) -> bool:
+        """True if appliance is air conditioner"""
+        return is_climate(self.appliance)
+
+    def is_dehumidifier(self) -> bool:
+        """True if appliance is dehumidifier"""
+        return is_dehumidifier(self.appliance)
+
+    @final
+    def dehumidifier(self) -> DehumidifierAppliance:
+        """Returns state as dehumidifier"""
+        return cast(DehumidifierAppliance, self.appliance.state)
+
+    @final
+    def airconditioner(self) -> AirConditionerAppliance:
+        """Returns state as air conditioner"""
+        return cast(AirConditionerAppliance, self.appliance.state)
 
 
 class AbstractHub(ABC):
@@ -110,3 +160,64 @@ class AbstractHub(ABC):
     @abstractmethod
     async def async_update_config(self) -> None:
         """Updates config entry from Hub's data"""
+
+
+class MideaClient:
+    """Delegate to midea API"""
+
+    def connect_to_cloud(  # pylint: disable=no-self-use
+        self, account: str, password: str, appkey=DEFAULT_APPKEY, appid=DEFAULT_APP_ID
+    ):
+        """Delegate to midea_beautiful_api.connect_to_cloud"""
+        return midea_beautiful_api.connect_to_cloud(
+            account=account, password=password, appkey=appkey, appid=appid
+        )
+
+    def appliance_state(  # pylint: disable=too-many-arguments,no-self-use
+        self,
+        address: str = None,
+        token: str = None,
+        key: str = None,
+        cloud: MideaCloud = None,
+        use_cloud: bool = False,
+        appliance_id: str = None,
+    ):
+        """Delegate to midea_beautiful_api.appliance_state"""
+        return midea_beautiful_api.appliance_state(
+            address=address,
+            token=token,
+            key=key,
+            cloud=cloud,
+            use_cloud=use_cloud,
+            appliance_id=appliance_id,
+            retries=5,
+            cloud_timeout=6,
+        )
+
+    def find_appliances(  # pylint: disable=too-many-arguments,no-self-use
+        self,
+        cloud: MideaCloud = None,
+        appkey: str = None,
+        account: str = None,
+        password: str = None,
+        appid: str = None,
+        addresses: list[str] = None,
+        retries: int = 3,
+        timeout: int = 3,
+    ) -> list[LanDevice]:
+        """Delegate to midea_beautiful_api.find_appliances"""
+        return midea_beautiful_api.find_appliances(
+            cloud=cloud,
+            appkey=appkey,
+            account=account,
+            password=password,
+            appid=appid,
+            addresses=addresses,
+            retries=retries,
+            timeout=timeout,
+        )
+
+
+def address_ok(address: str | None) -> bool:
+    """Returns True if address is not known"""
+    return address is not None and address != UNKNOWN_IP
