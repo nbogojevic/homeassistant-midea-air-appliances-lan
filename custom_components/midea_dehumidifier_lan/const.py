16d58fc1563051e367ee7ce389b64993906c68fa
Nenad Bogojevic <nbogojevic@hotmail.com>
2022-05-31 20:58:39 +0000

    Bump version: 0.8.3 → 0.8.9

--- custom_components/midea_dehumidifier_lan/const.py
+++ custom_components/midea_dehumidifier_lan/const.py
@@ -11,7 +11,7 @@ from midea_beautiful.midea import (
     DEFAULT_APP as DEFAULT_APP_FROM_LIB,
 )
 
-__version__ = "0.8.3"
+__version__ = "0.8.9"
 
 # Base component constants
 NAME: Final = "Midea Air Appliance (LAN)"
