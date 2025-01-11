dc740fc1b6527e2ef43acbe280f9582adb8c4644
Hugo Pereira <sindamc@gmail.com>
2025-01-11 13:56:27 +0000

    Update const.py

--- custom_components/midea_dehumidifier_lan/const.py
+++ custom_components/midea_dehumidifier_lan/const.py
@@ -11,7 +11,7 @@ from midea_beautiful.midea import (
     DEFAULT_APP as DEFAULT_APP_FROM_LIB,
 )
 
-__version__ = "0.9.4"
+__version__ = "0.9.5"
 
 # Base component constants
 NAME: Final = "Midea Air Appliance (LAN)"
